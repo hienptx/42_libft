e92128b9a4b67529d03840d468de0bb06b7866c9
Mafalda <147611378+hienptx@users.noreply.github.com>
2023-12-26 14:38:09 +0100

    Update ft_bzero.c
    Function description

--- ft_bzero.c
+++ ft_bzero.c
@@ -10,6 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+The bzero() function writes n zeroed bytes to the string s.  If n is zero, bzero() does nothing.
+**/
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
