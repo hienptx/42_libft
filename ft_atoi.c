782e56181a2ff1b275f2979ddb858a8c90583b9f
Mafalda <147611378+hienptx@users.noreply.github.com>
2023-12-26 14:29:18 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -22,9 +22,7 @@ int	ft_atoi(const char *str)
 	neopo = 1;
 	output = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-	{
 		i++;
-	}
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
